75058af3512be2aa4cbb285d369c651513c674c7
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-12-23 11:06:33 +0100

    add graph

---
+++ utils/graph.js
@@ -0,0 +1,62 @@
+export const GithubContributionGraph = (props) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    className="block" 
+    width="100%" 
+    height="100%" 
+    viewBox="0 0 185.09 184.09"
+  >
+    <g>
+      <path
+        strokeLinejoin="round"
+        fill="rgb(239 68 68)"
+        stroke="rgb(239 68 68 / 0.5)"
+        opacity="0.5"
+        strokeWidth="10"
+        d="M92.5,92.5 L93.45882161458331,92.5 L92.5,95.37646484375 L4.453125,92.5 z"
+      ></path>
+      <line
+        strokeWidth="4"
+        strokeLinecap="round"
+        stroke="rgb(239 68 68)"
+        x1="0.453125"
+        y1="92.5"
+        x2="184.546875"
+        y2="92.5"
+      ></line>
+      <line
+        strokeWidth="4"
+        strokeLinecap="round"
+        stroke="rgb(239 68 68)"
+        x1="92.5"
+        y1="0.453125"
+        x2="92.5"
+        y2="184.546875"
+      ></line>
+      <ellipse
+        rx="3"
+        ry="3"
+        strokeWidth="4"
+        fill="rgb(254 226 226)"
+        cx="95.45882161458331"
+        cy="92.5"
+      ></ellipse>
+      <ellipse
+        rx="3"
+        ry="3"
+        strokeWidth="4"
+        fill="rgb(254 226 226)"
+        cx="92.5"
+        cy="97.37646484375"
+      ></ellipse>
+      <ellipse
+        rx="3"
+        ry="3"
+        strokeWidth="4"
+        fill="rgb(254 226 226)"
+        cx="2.453125"
+        cy="92.5"
+      ></ellipse>
+    </g>
+  </svg>
+);
